6d5300dba2159e5c29609486480551419c62af2c
Olya <122615170+olyalupatsa@users.noreply.github.com>
2023-04-30 10:57:25 +0300

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]")
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.modal.addEventListener("click", (click) => {
+        if (click.target === click.currentTarget) toggleModal();
+    });
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+    }
+})();
